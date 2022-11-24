f1f9d582efbc9e1c94d7e96aab5fd13856486e00
Inhwan Wee <10ezenfinani@gmail.com>
2022-11-25 08:57:15 +0900

    iOS C interop

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,8 +31,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _nativeAddPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _nativeAddPlugin.getPlatformVersion() ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -55,7 +55,13 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Running on: $_platformVersion'),
+              Text('1 + 2 == ${nativeAdd(1, 2)}'),
+            ],
+          ),
         ),
       ),
     );
