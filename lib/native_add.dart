fce5761435beeeacf9e28467d06c704c13c5a797
inhwan.wee <inhwan.wee@nearthlab.com>
2022-11-28 11:15:04 +0900

    android C interop

--- lib/native_add.dart
+++ lib/native_add.dart
@@ -6,21 +6,18 @@ import 'package:path/path.dart' as path;
 final String libraryPath = Platform.isLinux
     ? path.join(Directory.current.parent.path, 'cppRobotics', 'build',
         'libnative_add.so')
-    : "";
+    : Platform.isAndroid
+        ? 'libnative_add.so'
+        : "";
 
 // final String libraryPath = Platform.isWindows
 //     ? path.join(Directory.current.parent.path, 'cppRobotics', 'build', 'Debug',
 //         'hello.dll')
 //     : "";
 
-final DynamicLibrary cppRoboticsLib = DynamicLibrary.open(libraryPath);
-
-// final DynamicLibrary nativeAddLib = Platform.isAndroid
-//     ? DynamicLibrary.open('libnative_add.so')
-//     : DynamicLibrary.process();
-
-// iOS, macOS
-// DynamicLibrary.process();
+final DynamicLibrary cppRoboticsLib = (Platform.isIOS || Platform.isMacOS)
+    ? DynamicLibrary.process()
+    : DynamicLibrary.open(libraryPath);
 
 final int Function(int x, int y) nativeAdd = cppRoboticsLib
     .lookup<NativeFunction<Int32 Function(Int32, Int32)>>('native_add')
