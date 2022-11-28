892f25fbf223c74cfb43a2bb0df0af2ca3e9f231
inhwan.wee <inhwan.wee@nearthlab.com>
2022-11-28 10:21:06 +0900

    rename cppRobotics CMakeLists, library path

---
+++ cppRobotics/src/main.cpp
@@ -0,0 +1,7 @@
+#include "native_add.h"
+
+int main()
+{
+    native_add(1, 2);
+    return 0;
+}
--- lib/native_add.dart
+++ lib/native_add.dart
@@ -3,14 +3,24 @@ import 'dart:ffi'; // For FFI
 import 'dart:io'; // For Platform.isX
 import 'package:path/path.dart' as path;
 
-final String libraryPath = path.join(
-    Directory.current.parent.path, 'cppRobotics', 'build', 'libnative_add.so');
+final String libraryPath = Platform.isLinux
+    ? path.join(Directory.current.parent.path, 'cppRobotics', 'build',
+        'libnative_add.so')
+    : "";
+
+// final String libraryPath = Platform.isWindows
+//     ? path.join(Directory.current.parent.path, 'cppRobotics', 'build', 'Debug',
+//         'hello.dll')
+//     : "";
 
 final DynamicLibrary cppRoboticsLib = DynamicLibrary.open(libraryPath);
 
 // final DynamicLibrary nativeAddLib = Platform.isAndroid
 //     ? DynamicLibrary.open('libnative_add.so')
 //     : DynamicLibrary.process();
+
+// iOS, macOS
+// DynamicLibrary.process();
 
 final int Function(int x, int y) nativeAdd = cppRoboticsLib
     .lookup<NativeFunction<Int32 Function(Int32, Int32)>>('native_add')
