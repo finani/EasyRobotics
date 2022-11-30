0d772c0beafd2d722bfc555580d23970d5802a74
Inhwan Wee <10ezenfinani@gmail.com>
2022-11-30 09:38:56 +0900

    fix extern C(iOS, macOS)

--- cppRobotics/src/native_add.h
+++ cppRobotics/src/native_add.h
@@ -1,7 +1,15 @@
 #pragma once
 
-extern "C" {
   #include <stdint.h>
 
+#ifdef _WIN32
+  extern "C" {
+#else
+  extern "C" __attribute__((visibility("default"))) __attribute__((used))
+#endif
+
   int32_t native_add(int32_t x, int32_t y);
+
+#ifdef _WIN32
 }
+#endif
