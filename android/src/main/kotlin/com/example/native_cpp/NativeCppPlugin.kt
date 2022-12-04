2601c72a889574412d4a9df5a9c85861897ea988
finani <10ezenfinani@gmail.com>
2022-12-04 22:10:34 +0900

    rename native_add to native_cpp (android)

--- android/src/main/kotlin/com/example/native_cpp/NativeCppPlugin.kt
+++ android/src/main/kotlin/com/example/native_cpp/NativeCppPlugin.kt
@@ -1,4 +1,4 @@
-package com.example.native_add
+package com.example.native_cpp
 
 import androidx.annotation.NonNull
 
@@ -8,8 +8,8 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-/** NativeAddPlugin */
-class NativeAddPlugin: FlutterPlugin, MethodCallHandler {
+/** NativeCppPlugin */
+class NativeCppPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -17,7 +17,7 @@ class NativeAddPlugin: FlutterPlugin, MethodCallHandler {
   private lateinit var channel : MethodChannel
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "native_add")
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "native_cpp")
     channel.setMethodCallHandler(this)
   }
 
