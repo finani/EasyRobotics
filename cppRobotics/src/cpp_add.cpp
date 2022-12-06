de2079399883c18b24961c6cd31052255d9482a4
Inhwan Wee <10ezenfinani@gmail.com>
2022-12-06 09:55:38 +0900

    add format.sh (cppRobotics)

--- cppRobotics/src/cpp_add.cpp
+++ cppRobotics/src/cpp_add.cpp
@@ -1,5 +1,3 @@
 #include "cpp_add.h"
 
-int32_t cpp_add(int32_t x, int32_t y) {
-    return x + y;
-}
+int32_t cpp_add(int32_t x, int32_t y) { return x + y; }
--- cppRobotics/src/cpp_add.h
+++ cppRobotics/src/cpp_add.h
@@ -1,19 +1,19 @@
 #pragma once
 
-  #include <stdint.h>
+#include <stdint.h>
 
 #if defined(__cplusplus)
 #ifdef _WIN32
-  extern "C" {
+extern "C" {
 #else
-  extern "C" __attribute__((visibility("default"))) __attribute__((used))
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
 #endif
 #endif
 
-  int32_t cpp_add(int32_t x, int32_t y);
+int32_t cpp_add(int32_t x, int32_t y);
 
 #if defined(__cplusplus)
 #ifdef _WIN32
-}  // extern "C"
+} // extern "C"
 #endif
 #endif
