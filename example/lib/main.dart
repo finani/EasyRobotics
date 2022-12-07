747abdc751635af48aea724bb047b37c53eafc50
finani <10ezenfinani@gmail.com>
2022-12-08 01:01:07 +0900

    add syncfusion, first order filter plot

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:flutter/services.dart';
 import 'package:native_cpp/native_cpp.dart';
 import 'package:native_cpp/cpp_add.dart';
@@ -21,11 +22,16 @@ class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   final _nativeCppPlugin = NativeCpp();
 
+  Timer? _timer;
+  final List<_ChartData> _chartData = [];
+  double _step = 0.0;
+
   @override
   void initState() {
     super.initState();
     initPlatformState();
 
+    firstOrderFilterResetFilter();
     const timeConstantSec = 0.1;
     firstOrderFilterSetParams(0.0, timeConstantSec);
   }
@@ -54,6 +60,13 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    _getChartData();
+    _timer = Timer(const Duration(seconds: 1), () {
+      setState(() {
+        _getChartData();
+      });
+    });
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -65,20 +78,63 @@ class _MyAppState extends State<MyApp> {
             children: [
               Text('Running on: $_platformVersion'),
               Text('1 + 2 == ${cppAdd(1, 2)}'),
-              Text('1 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('2 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('3 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('4 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('5 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('6 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('7 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('8 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('9 step = ${firstOrderFilterCalc(1.0)}'),
-              Text('10 step = ${firstOrderFilterCalc(1.0)}'),
+              _buildAnimationLineChart(),
             ],
           ),
         ),
       ),
     );
   }
+
+  ///Get the cartesian chart with line series
+  SfCartesianChart _buildAnimationLineChart() {
+    return SfCartesianChart(
+        plotAreaBorderWidth: 0,
+        primaryXAxis:
+            NumericAxis(majorGridLines: const MajorGridLines(width: 0)),
+        primaryYAxis: NumericAxis(
+            majorTickLines: const MajorTickLines(color: Colors.transparent),
+            axisLine: const AxisLine(width: 0),
+            minimum: 0,
+            maximum: 1),
+        series: _getDefaultLineSeries());
+  }
+
+  /// The method returns line series to chart.
+  List<LineSeries<_ChartData, num>> _getDefaultLineSeries() {
+    return <LineSeries<_ChartData, num>>[
+      LineSeries<_ChartData, num>(
+          dataSource: _chartData,
+          xValueMapper: (_ChartData sales, _) => sales.x,
+          yValueMapper: (_ChartData sales, _) => sales.y,
+          markerSettings: const MarkerSettings(isVisible: true)),
+      LineSeries<_ChartData, num>(
+          dataSource: _chartData,
+          xValueMapper: (_ChartData sales, _) => sales.x,
+          yValueMapper: (_ChartData sales, _) => sales.y2,
+          markerSettings: const MarkerSettings(isVisible: true))
+    ];
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+    _chartData.clear();
+  }
+
+  void _getChartData() {
+    const input = 1.0;
+    final output = firstOrderFilterCalc(input);
+    _chartData.add(_ChartData(_step, output, input));
+    _step += 1;
+    _timer?.cancel();
+  }
+}
+
+class _ChartData {
+  _ChartData(this.x, this.y, this.y2);
+  final double x;
+  final double y;
+  final double y2;
 }
