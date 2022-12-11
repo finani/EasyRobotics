c39f0af3198fcea9d25649bd8096256949727d41
finani <10ezenfinani@gmail.com>
2022-12-12 01:02:59 +0900

    add qr code

--- example/lib/src/pages/info.dart
+++ example/lib/src/pages/info.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_flutter/qr_flutter.dart';
+
 class Info extends StatelessWidget {
   const Info({
     super.key,
@@ -14,6 +16,35 @@ class Info extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Index 2: Info"),
         backgroundColor: appBarColor,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(flex: 2),
+            const Text(
+              "Easy Robotics",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const Text("github"),
+            const Spacer(),
+            QrImage(
+              version: 6,
+              // embeddedImage: , You can add your custom image to the center of your QR
+              // semanticsLabel:'', You can add some info to display when your QR scanned
+              backgroundColor: Colors.transparent,
+              foregroundColor: Colors.black,
+              errorCorrectionLevel: QrErrorCorrectLevel.M,
+              // padding: const EdgeInsets.all(5),
+              size: 300,
+              data: "https://github.com/finani/EasyRobotics",
+            ),
+            const Spacer(flex: 2),
+          ],
+        ),
       ),
     );
   }
