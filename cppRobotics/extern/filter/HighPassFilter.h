e59c8fe4a25181c19ecf0fcd613c0fa1d9210d84
Inhwan Wee <10ezenfinani@gmail.com>
2022-12-13 11:11:00 +0900

    add high pass filter (extern)

---
+++ cppRobotics/extern/filter/HighPassFilter.h
@@ -0,0 +1,19 @@
+#include "../../src/filter/HighPassFilter.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+double HighPassFilterCalc(double curInput);
+
+double* HighPassFilterGetParams();
+
+void HighPassFilterSetParams(double cutOffFreqHz, double timeConstantSec);
+
+void HighPassFilterResetFilter();
+
+void HighPassFilterResetPrevValues();
+
+#if defined(__cplusplus)
+} // extern "C"
+#endif
