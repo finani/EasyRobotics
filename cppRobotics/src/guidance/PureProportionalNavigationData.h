e6f1747b92cdeda61bcacd443f718dcd16831a06
finani <10ezenfinani@gmail.com>
2022-12-15 20:20:08 +0900

    apply eigen vector3d

--- cppRobotics/src/guidance/PureProportionalNavigationData.h
+++ cppRobotics/src/guidance/PureProportionalNavigationData.h
@@ -1,14 +1,14 @@
 #ifndef GUIDANCE_PURE_PROPORTIONAL_NAVIGATION_INPUT_DATA_H
 #define GUIDANCE_PURE_PROPORTIONAL_NAVIGATION_INPUT_DATA_H
 
-#include <array>
+#include <eigen3/Eigen/Dense>
 
 struct PureProportionalNavigationData {
-  std::array<double, 3> curPos;
-  std::array<double, 3> targetPos;
+  Eigen::Vector3d curPos;
+  Eigen::Vector3d targetPos;
 
-  PureProportionalNavigationData(const std::array<double, 3> &curPos,
-                                 const std::array<double, 3> &targetPos)
+  PureProportionalNavigationData(const Eigen::Vector3d &curPos,
+                                 const Eigen::Vector3d &targetPos)
       : curPos(curPos), targetPos(targetPos) {}
 };
 
